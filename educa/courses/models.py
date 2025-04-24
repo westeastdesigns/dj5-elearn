65a8c939a309c96615e3d64edcaf3cf6a8fe220a
westeastdesigns <47990949+westeastdesigns@users.noreply.github.com>
2025-04-23 20:44:42 -0400

    created content models and ItemBase abstract model

--- educa/courses/models.py
+++ educa/courses/models.py
@@ -94,7 +94,7 @@ class Content(models.Model):
     Args:
         models 
         module (ForeignKey): a module contains multiple contents, ForeignKey points to Module model
-        content_type (ForeignKey): ForeignKey points to ContentType model
+        content_type (ForeignKey): ForeignKey points to ContentType model. Limited ContentType objects to text, video, image, and file
         object_id (PositiveIntegerField): stores primary key of the related object
         
         item (GenericForeignKey): GenericForeignKey field to the related object combining the two previous fields 
@@ -106,8 +106,71 @@ class Content(models.Model):
     )
     content_type = models.ForeignKey(
         ContentType,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        limit_choices_to={
+            'model__in':('text', 'video', 'image', 'file')
+        }
     )
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey('content_type', 'object_id')
-    
+
+
+class ItemBase(models.Model):
+    """ItemBase is an abstract model defining the owner, title, created, and updated fields. All types of content use these common fields.
+
+    Args:
+        models 
+        owner (ForeignKey): stores which user created the content, with a different related_name auto-generated for each sub-model
+        title (CharField): title of content
+        created (DateTimeField): automatically generated date and time the content was created
+        updated (DateTimeField): automatically generated date and time of most recent update to content
+
+    Returns:
+        str: title
+    """
+    owner = models.ForeignKey(
+        User,
+        related_name='%(class)s_related',
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+class Text(ItemBase):
+    """Text inherits from abstract model ItemBase, stores text content
+
+    Args:
+        ItemBase (TextField): text content
+    """
+    content = models.TextField()
+
+class File(ItemBase):
+    """File inherits from abstract model ItemBase, stores files such as PDFs. Does not include image or video files.
+
+    Args:
+        ItemBase (FileField): file content, for example: a PDF
+    """
+    file = models.FileField(upload_to='files')
+
+class Image(ItemBase):
+    """Image inherits from abstract model ItemBase, stores image files
+
+    Args:
+        ItemBase (FileField): image files, for example: a PNG
+    """
+    file = models.FileField(upload_to='images')
+
+class Video(ItemBase):
+    """Video inherits from abstract model ItemBase, stores videos. Uses URLField to provide a video URL for embedding
+
+    Args:
+        ItemBase (URLField): provides a video URL in order to embed it
+    """
+    url = models.URLField()
