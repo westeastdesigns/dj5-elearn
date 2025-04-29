536d731de8415e5f3342824755047651e8208fae
westeastdesigns <47990949+westeastdesigns@users.noreply.github.com>
2025-04-28 21:05:58 -0400

    added a formset to course modules

--- educa/courses/urls.py
+++ educa/courses/urls.py
@@ -24,4 +24,9 @@ urlpatterns = [
         views.CourseDeleteView.as_view(),
         name='course_delete',
     ),
+    path(
+        '<pk>/module/',
+        views.CourseModuleUpdateView.as_view(),
+        name='course_module_update'
+    ),
 ]
